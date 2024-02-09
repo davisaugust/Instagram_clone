b75ea23ce7398c21b8250c3b85836a0d6091f471
davisaugust <davisaugust004@gmail.com>
2024-02-08 22:49:44 -0300

    Style: Carousel adjustments.

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,39 @@
-const arrowbtn = document.getElementsByClassName('.arrow-btn');
+document.addEventListener("DOMContentLoaded", function() {
+    const prevBtn = document.querySelector(".prev");
+    const nextBtn = document.querySelector(".next");
+    const slides = document.querySelector(".slides");
+    const slideItems = document.querySelectorAll(".slides li");
+    const slideWidth = slideItems[0].offsetWidth;
+    let slideIndex = 0;
 
-arrowbtn.addEventListener('click', () => {
-    console.log('funciona')
-})
+    function updateButtonsVisibility() {
+        if (slideIndex === 0) {
+            prevBtn.style.display = "none";
+        } else {
+            prevBtn.style.display = "block";
+        }
+        if (slideIndex === slideItems.length - 1) {
+            nextBtn.style.display = "none";
+        } else {
+            nextBtn.style.display = "block";
+        }
+    }
+
+    nextBtn.addEventListener("click", () => {
+        if (slideIndex < slideItems.length - 1) {
+            slideIndex++;
+            slides.style.transform = `translateX(-${slideWidth * slideIndex}px)`;
+            updateButtonsVisibility();
+        }
+    });
+
+    prevBtn.addEventListener("click", () => {
+        if (slideIndex > 0) {
+            slideIndex--;
+            slides.style.transform = `translateX(-${slideWidth * slideIndex}px)`;
+            updateButtonsVisibility();
+        }
+    });
+
+    updateButtonsVisibility();
+});
